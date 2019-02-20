2707ce22a44e42270f35e8c1d34191f360f15a81
Krzysztof Syrytczyk <krzysztof.syrytczyk@gmail.com>
2019-02-20 22:24:27 +0100

    Create shared module for shared parts of application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppLayoutsModule } from './app-layouts/app-layouts.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
     declarations: [
@@ -12,7 +13,8 @@ import { AppLayoutsModule } from './app-layouts/app-layouts.module';
     imports: [
         BrowserModule,
         AppRoutingModule,
-        AppLayoutsModule
+        AppLayoutsModule,
+        SharedModule
     ],
     providers: [],
     bootstrap: [AppComponent]
