30bf295d8c081a915e21041c9cec932e13236a70
Krzysztof Syrytczyk <krzysztof.syrytczyk@gmail.com>
2019-02-20 23:23:13 +0100

    Create simple request service for external API

---
+++ src/app/shared/request/request.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+export interface FetchListResponseBody {
+    id: number;
+    title: string;
+    rating: number;
+    votes: number;
+    posterSrc: string;
+    genres: Array<string>;
+    releaseDate: string;
+}
+
+export interface FetchMovieResponseBody {
+    id: number;
+    title: string;
+    budget: number;
+    description: string;
+    rating: number;
+    votes: number;
+    posterSrc: string;
+    homepage?: string;
+    genres: Array<string>;
+    duration: number;
+    videos: Array<{
+        name: string;
+        size: number;
+        type: string;
+        url: string;
+    }>;
+}
+
+const API_URL = 'http://vps135320.ovh.net:1234';
+const API_METHODS = {
+    fetchList: `${API_URL}/list/:page`,
+    fetchMovie: `${API_URL}/movie/:id`
+};
+
+@Injectable({
+    providedIn: 'root'
+})
+export class RequestService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    public fetchList(page = 1): Observable<FetchListResponseBody> {
+        return this.makeRequest(API_METHODS.fetchList, {
+            page
+        }) as Observable<FetchListResponseBody>;
+    }
+
+    public fetchMovie(id: number): Observable<FetchMovieResponseBody> {
+        return this.makeRequest(API_METHODS.fetchMovie, {
+            id
+        }) as Observable<FetchMovieResponseBody>;
+    }
+
+    private makeRequest(url, params = {}): Observable<object> {
+        Object.entries(params)
+            .forEach(([key, value]) => {
+                url = url.replace(`:${key}`, value);
+            });
+
+        return this.http.get(url);
+    }
+
+}
